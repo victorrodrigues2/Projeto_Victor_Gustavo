357a7c2a8ed93d1c6279d7dacbf21af0603e10fb
victorrodrigues2 <66637518+victorrodrigues2@users.noreply.github.com>
2020-06-08 14:17:06 -0300

    Add files via upload

---
+++ A.java
@@ -0,0 +1,36 @@
+public class A {
+	
+		
+	int A1;
+	float A2;
+	
+	public static void main (String[] args) {
+	
+	}
+	
+	public void setA1(int umA1){
+		this.A1 = umA1;
+	}
+	
+	public void setA2(int umA2){
+		this.A2 = umA2;
+	}
+	
+	public int getA1(){
+		return A1;
+	}
+	
+	public float getA2(){
+		return A2;
+	}
+	
+	public String MA1(){
+		return ("MA1");
+	}
+	
+	public String MA2(){
+		return ("MA1");
+	}
+	
+}
+
---
+++ B.java
@@ -0,0 +1,33 @@
+public class B {
+	
+	int B1;
+	float B2;
+	
+	public static void main (String[] args) {
+		
+	}
+	
+	public void setB1(int umB1){
+		this.B1 = umB1;
+	}
+	
+	public void setB2(int umB2){
+		this.B2 = umB2;
+	}
+	
+	public int getB1(){
+		return B1;
+	}
+	
+	public float getB2(){
+		return B2;
+	}
+	
+	public String MB1(){
+		return ("MB1");
+	}
+	
+	public String MB2(){
+		return ("MB1");
+	}
+}
